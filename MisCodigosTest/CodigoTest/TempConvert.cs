53dc1fb270b57ff2dd2866710210e83b554c7b58
MemoRodz <gmo.rodriguez@gmail.com>
2024-01-24 13:31:16 -0600

    Conversión de Temperatura °C a °F y viceversa

---
+++ MisCodigosTest/CodigoTest/TempConvert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisCodigosTest.CodigoTest
+{
+    public class TempConvert
+    {
+        static decimal celsiusF(decimal fahrenheit)
+        {
+            return (fahrenheit - 32m) * (5m / 9m);
+        }
+
+        static decimal fahrenheit(decimal celsius)
+        {
+            return celsius * 9 / 5 + 32;
+        }
+    }
+}
